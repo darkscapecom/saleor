732ea3d1d4ab3f1e1f8fc4ce99be21f0c01810ba
darkscapecom <4@darkscape.com>
2026-05-24 17:16:28 -0700

    Create clean Saleor 3.23.7 branch

---
+++ saleor/webhook/transport/synchronous/__init__.py
@@ -0,0 +1,9 @@
+from .transport import (
+    trigger_webhook_sync,
+    trigger_webhook_sync_if_not_cached,
+)
+
+__all__ = [
+    "trigger_webhook_sync",
+    "trigger_webhook_sync_if_not_cached",
+]
